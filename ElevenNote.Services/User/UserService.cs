9e8a4cd402507fe62457bc77bcfab9ae9ddcba12
Scott Allen <sallen1851@gmail.com>
2023-11-06 17:00:05 -0500

    added routes

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,7 +1,42 @@
+using ElevenNote.Models.User;
+using ElevenNote.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User;
 
 public class UserService : IUserService
 {
+    private readonly ApplicationDbContext _context;
+    public UserService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<bool> RegisterUserAsync(UserRegister model)
+    {
+        if (await GetUserByEmailAsync(model.Email) != null || await GetUserByUsernameAsync(model.Username) != null)
+            return false;
 
+        UserEntity entity = new()
+        {
+            Email = model.Email, 
+            Username = model.Username, 
+            Password = model.Password, 
+            DateCreated = DateTime.Now
+        };
+
+        _context.Users.Add(entity);
+        int numberOfChanges = await _context.SaveChangesAsync();
+
+        return numberOfChanges == 1;
+    }
+
+}
+
+private async Task<UserEntity?> GetUserByEmailAsync(string email)
+{
+    return await _context.Users.FirstOrDefaultAsync(User => User.Email.ToLower() == email.ToLower());
+}
+private async Task<UserEntity?> GetUserByUsernameAsync(string username)
+{
+    return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
 }
