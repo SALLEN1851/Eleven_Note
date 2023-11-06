25572c13fa976cf46dd0c38b9c6bc62c3faa38ea
Scott Allen <sallen1851@gmail.com>
2023-11-06 15:46:40 -0500

    defined model

---
+++ ElevenNote.Models/User/UserRegister.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations:
+
+namespace ElevenNote.Models.User;
+
+public class UserRegister
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(4)]
+    public string Username { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(4)]
+    public string Password { get; set; } = string.Empty;
+    
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
